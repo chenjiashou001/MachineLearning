d0c7b27fe1ddd9012c24f2a5f2e194ad43429bb6
New_Life <chenhuan001@jiashou.local>
2017-03-11 16:04:41 +0800

    Initial Commit

---
+++ MachineLearning/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  MachineLearning
+//
+//  Created by New_Life on 17/3/11.
+//  Copyright © 2017年 chenhuan001. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
