9ad3c960946adc00dd20d0839e6b741665d99bea
New_Life <chenhuan001@jiashou.local>
2017-03-12 21:08:03 +0800

    change

---
+++ MachineLearning/IData.cpp
@@ -0,0 +1,9 @@
+//
+//  IData.cpp
+//  MachineLearning
+//
+//  Created by New_Life on 17/3/12.
+//  Copyright © 2017年 chenhuan001. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ MachineLearning/NavieByes.hpp
@@ -0,0 +1,14 @@
+//
+//  NavieByes.hpp
+//  MachineLearning
+//
+//  Created by New_Life on 17/3/11.
+//  Copyright © 2017年 chenhuan001. All rights reserved.
+//
+
+#ifndef NavieByes_hpp
+#define NavieByes_hpp
+
+#include <stdio.h>
+
+#endif /* NavieByes_hpp */
